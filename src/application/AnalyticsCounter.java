52e4708e08411c6d705f161b98b6fb6ab2049984
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-12-01 14:52:12 +0100

    add method count

--- src/application/AnalyticsCounter.java
+++ src/application/AnalyticsCounter.java
@@ -1,19 +1,22 @@
 package application;
 
-
+import implement.ISymptomCount;
 import implement.ISymptomReader;
 import methods.ReadSymptomDataFromFile;
+import methods.CountSymptomDataFromReader;
 import java.util.List;
-
+import java.util.Map;
 
 public class AnalyticsCounter {
     public void analyse() {
 
         final ISymptomReader getSymptoms = new ReadSymptomDataFromFile();
+        final ISymptomCount countSymptoms = new CountSymptomDataFromReader();
 
 
         List<String> result = getSymptoms.getSymptoms("Openclassroom_Project_2/symptoms.txt");
         countSymptoms.countSymptoms(result);
+        Map<String, Integer> resultCount = countSymptoms.countSymptoms(result);
 
 
     }
